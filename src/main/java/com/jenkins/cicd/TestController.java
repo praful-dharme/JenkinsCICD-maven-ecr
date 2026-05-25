e44da38cf3d52f0ff08708d0b7d4391dcbff951b
praful-dharme <102966841+praful-dharme@users.noreply.github.com>
2026-05-25 16:12:01 +0530

    Update TestController.java-v4

--- src/main/java/com/jenkins/cicd/TestController.java
+++ src/main/java/com/jenkins/cicd/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 	@GetMapping("/test")
 	public String test()
 	{
-		return "Hello World! V3.........Devops_PD-with sonar-Qube";
+		return "Hello World! V4.........Devops_PD-with sonar-Qube";
 	}
 }
